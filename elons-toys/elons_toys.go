8990e630512927397555d4f43a0ab5750e9b5f50
Matej Pavic <matejpavic@Matejs-MacBook-Pro-2.local>
2024-01-03 10:14:07 +0100

    Add solution for the Elon toys task

--- elons-toys/elons_toys.go
+++ elons-toys/elons_toys.go
@@ -1,18 +1,25 @@
 package elon
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+func (c *Car) Drive() {
+	if c.battery < c.batteryDrain {
+		return
+	}
+	c.battery -= c.batteryDrain
+	c.distance += c.speed
+}
 
-// TODO: define the 'DisplayBattery() string' method
+func (c *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
+
+func (c *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
 
 // TODO: define the 'CanFinish(trackDistance int) bool' method
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-// 
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
-// functionality.
+func (c *Car) CanFinish(trackDistance int) bool {
+	return trackDistance-c.distance <= c.battery/c.batteryDrain*c.speed
+}

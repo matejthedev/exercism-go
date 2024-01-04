c0a728f4e5739f2f3c4064124e92a56856e75945
Matej Pavic <matejpavic@Matejs-MacBook-Pro-2.local>
2024-01-04 10:35:56 +0100

    Implement runes solution

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,33 @@
 package logs
 
-// Application identifies the application emitting the given log.
+var mapping = map[rune]string{
+	'❗': "recommendation",
+	'🔍': "search",
+	'☀': "weather",
+}
+
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, v := range log {
+		log, ok := mapping[v]
+		if ok {
+			return log
+		}
+	}
+	return "default"
 }
 
-// Replace replaces all occurrences of old with new, returning the modified log
-// to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	newLog := ""
+	for _, v := range log {
+		if v == oldRune {
+			newLog += string(newRune)
+		} else {
+			newLog += string(v)
+		}
+	}
+	return newLog
 }
 
-// WithinLimit determines whether or not the number of characters in log is
-// within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return len([]rune(log)) <= limit
 }

9b6d73af36b3d53ac338bf6410e840518cc652b6
Matej Pavic <matejpavic@Matejs-MacBook-Pro-2.local>
2024-01-06 12:11:46 +0100

    Add regex solution

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -1,21 +1,43 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	re := regexp.MustCompile(`<(?:[~=,*-]*)>`)
+	return re.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	count := 0
+	re := regexp.MustCompile(`(?i)"[^"]*password[^"]*"`)
+	for _, v := range lines {
+		str := re.FindAllString(v, -1)
+		count += len(str)
+	}
+	return count
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	re := regexp.MustCompile(`end-of-line\d*(\s*)`)
+	return re.ReplaceAllString(text, "$1")
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	regex := regexp.MustCompile(`User\s+(\w*)`)
+	newLines := make([]string, len(lines))
+	copy(newLines, lines)
+	for i, line := range newLines {
+		if submatches := regex.FindStringSubmatch(line); submatches != nil {
+			newLines[i] = fmt.Sprintf("[USR] %s %s", submatches[1], line)
+		}
+	}
+	return newLines
 }

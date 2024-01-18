1a68615f474756e5735a99aff08e11ac49ba5d01
Matej Pavic <matejpavic@Matejs-MBP-2.fritz.box>
2024-01-18 09:15:25 +0100

    Add solution for the zero values task

--- census/census.go
+++ census/census.go
@@ -8,22 +8,30 @@ type Resident struct {
 	Address map[string]string
 }
 
-// NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
 }
 
-// HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	return r.Name != "" && r.Address["street"] != ""
 }
 
-// Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	panic("Please implement Delete.")
+	r.Address = nil
+	r.Age = 0
+	r.Name = ""
 }
 
-// Count counts all residents that have provided the required information.
 func Count(residents []*Resident) int {
-	panic("Please implement Count.")
+	count := 0
+	for _, v := range residents {
+		if v.HasRequiredInfo() {
+			count++
+		}
+	}
+	return count
 }
